4970c0c23485993ec6dca3de601b93fb8f49e6a4
U-taro-ogw <51327986+U-taro-ogw@users.noreply.github.com>
2019-11-13 23:39:32 +0900

    外部APIへのfetch

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 void main() => runApp(MyApp());
 
@@ -23,14 +25,62 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+class Issue {
+  Issue({
+    this.title,
+    this.avatarUrl,
+  });
+
+  final String title;
+  final String avatarUrl;
+}
+
 class _MyHomePageState extends State<MyHomePage> {
+  List<Issue> _issues = <Issue>[];
+
+  @override
+  void initState() {
+    super.initState();
+    _load();
+  }
+
+  Future<void> _load() async {
+    final res = await http.get('https://api.github.com/repositories/31792824/issues');
+    final data = json.decode(res.body);
+    setState(() {
+      final issues = data as List;
+      issues.forEach((dynamic element){
+        final issue = element as Map;
+        _issues.add(Issue(
+          title: issue['title'] as String,
+          avatarUrl: issue['user']['avatar_url'] as String,
+        ));
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Text('空っぽの画面です'),
+      body: ListView.builder(
+          itemBuilder: (BuildContext context, int index) {
+            if (index >= _issues.length) {
+              return null;
+            }
+
+            final issue = _issues[index];
+
+            return ListTile(
+              leading: ClipOval(
+                child: Image.network(issue.avatarUrl),
+              ),
+              title: Text(issue.title),
+            );
+          },
+      )
     );
   }
 }
